d41c068dd389993adb88713b1c091a22fb666f7c
Mike McNeil <mike@balderdash.co>
2014-04-06 16:35:02 -0500

    Define Adapter constructor, other cleanup of ORM.

---
+++ docs/orm.js
@@ -0,0 +1,11 @@
+
+// Example usage:
+//
+// orm.identifyModel('Foo', { attributes: {}, database: 'larrysMysql' });
+// orm.forgetModel('Foo');
+//
+// orm.identifyDatabase('larrysMysql', { adapter: 'sails-mysql' });
+// orm.forgetDatabase('larrysMysql');
+//
+// orm.identifyAdapter('sails-mysql', require('sails-mysql'));
+// orm.forgetAdapter('sails-mysql');
