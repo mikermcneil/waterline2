ab5303140b7551004443975f61b0decdf1adcdc3
Mike McNeil <mike@balderdash.co>
2014-04-06 17:24:51 -0500

    Fix typo, remove log in test.

--- test/unit/ORM/refresh.test.js
+++ test/unit/ORM/refresh.test.js
@@ -21,9 +21,14 @@ describe('ORM', function () {
     });
 
     it('should work w/ a few models', function () {
-      orm.identifyModel('User');
+      orm.identifyModel('Foo');
+      orm.identifyModel('Bar', {});
+      orm.identifyModel('Baz', { attributes: {} });
+      orm.identifyModel({ identity: 'Bazket' });
+      orm.identifyModel('Weaving');
       var schema = orm.refresh();
       assert(typeof schema === 'object');
+      assert(typeof schema.foo === 'object');
     });
 
   });
