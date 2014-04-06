2e3706b44662ff6015a8ac7157f5ddb8be07d1d1
Mike McNeil <mike@balderdash.co>
2014-04-06 08:49:12 -0500

    Naming

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-module.exports = require('./lib/Waterline');
+/**
+ * Module dependencies
+ */
+
+var ORM = require('./lib/ORM');
+
+
+
+/**
+ * Factory which returns an ORM instance.
+ * @param  {Object} opts
+ * @return {ORM}
+ */
+
+module.exports = function Waterline (opts) {
+  return new ORM(opts);
+};
