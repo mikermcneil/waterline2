a1ae566d21facc143d560dc1646f945890283058
Mike McNeil <mike@balderdash.co>
2014-04-06 18:05:31 -0500

    Made the nitpickyass tests pass.

--- test/unit/ORM/transaction.test.js
+++ test/unit/ORM/transaction.test.js
@@ -4,7 +4,7 @@
 
 var assert = require('assert');
 var ORM = require('../../../lib/ORM');
-
+var _ = require('lodash');
 
 
 describe('ORM', function () {
@@ -37,39 +37,39 @@ describe('ORM', function () {
       // Need to specify an array of Models
       assert.throws(function () {
         orm.transaction();
-      });
+      }, 'Should throw when missing array of Model instances');
       assert.throws(function () {
         orm.transaction(function(){});
-      });
+      }, 'Should throw when missing array of Model instances');
       assert.throws(function () {
         orm.transaction(User, function(){});
-      });
+      }, 'Should throw when missing array of Model instances');
 
 
       // Need to specify a transaction function
       assert.throws(function () {
         orm.transaction([]);
-      });
+      }, 'Should throw when missing transaction function');
       assert.throws(function () {
         orm.transaction(User);
-      });
+      }, 'Should throw when missing transaction function');
       assert.throws(function () {
         orm.transaction([User]);
-      });
+      }, 'Should throw when missing transaction function');
 
 
-      // Strings don't work
+      // Strings don't work - they need to be actual Model instances
       assert.throws(function () {
         orm.transaction(['User']);
-      });
+      }, 'Should throw if strings are used instead of actual Model instances');
       assert.throws(function () {
         orm.transaction('User', function(){});
-      });
+      }, 'Should throw if strings are used instead of actual Model instances');
       assert.throws(function () {
         orm.transaction(['User'], function(){});
-      });
-
+      }, 'Should throw if strings are used instead of actual Model instances');
     });
+
 
     it('should work w/ most minimal valid usage', function (done) {
       orm.transaction([], function start (cb) {
