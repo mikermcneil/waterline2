a675524b5f6f900531dfa46141ed932d7cb77e36
Mike McNeil <mike@balderdash.co>
2014-04-06 16:55:31 -0500

    tests

---
+++ test/unit/ORM/constructor.test.js
@@ -0,0 +1,17 @@
+/**
+ * Module dependencies
+ */
+
+var ORM = require('../../../lib/ORM');
+
+
+
+describe('ORM', function () {
+  describe('constructor', function () {
+
+    it('should construct an ORM', function () {
+      var orm = new ORM();
+
+    });
+  });
+});
