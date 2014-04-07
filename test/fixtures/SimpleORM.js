0589d5a8bbfab40148550646a1c09cc037c94825
Mike McNeil <mike@balderdash.co>
2014-04-06 19:26:44 -0500

    Add first fixture and DDL tests.

---
+++ test/fixtures/SimpleORM.js
@@ -0,0 +1,60 @@
+/**
+ * Module dependencies
+ */
+
+var _ = require('lodash');
+_.defaults = require('merge-defaults');
+var assert = require('assert');
+var Waterline = require('../..');
+
+// helpers
+var isORM = require('../helpers/isORM');
+var isAdapter = require('../helpers/isAdapter');
+var isDatabase = require('../helpers/isDatabase');
+var isModel = require('../helpers/isModel');
+
+
+var DEFAULT_ONTOLOGY = {
+  models: {
+    user: {
+      database: 'default',
+      attributes: {}
+    }
+  },
+  databases: {
+    default: {
+      adapter: 'wl-pretend'
+    }
+  },
+  adapters: {
+    'wl-pretend': {
+      find: function (criteria, cb) { cb('not a real adapter'); }
+    }
+  }
+};
+
+
+/**
+ * Stub out an ORM instance with a simple default ontology.
+ * @return {ORM}
+ */
+
+module.exports = function SimpleORM (ontology) {
+  return Waterline(_.defaults(_.cloneDeep(DEFAULT_ONTOLOGY), ontology || {}));
+};
+
+
+
+describe('fixtures', function () {
+  describe('SimpleORM()', function () {
+    it('should return a sane, valid ORM instance', function () {
+      assert(isORM(module.exports()));
+    });
+
+    it('should return a properly configured ontology in that ORM instance', function () {
+      assert( isAdapter (_.find(module.exports().adapters, { identity: 'wl-pretend' })), 'adapter is missing or invalid' );
+      assert( isDatabase(_.find(module.exports().databases, { identity: 'default' })), 'database is missing or invalid' );
+      assert( isModel   (_.find(module.exports().models, { identity: 'user' })), 'model is missing or invalid' );
+    });
+  });
+});
--- test/integration/ddl.test.js
+++ test/integration/ddl.test.js
@@ -6,37 +6,48 @@ var assert = require('assert');
 var _ = require('lodash');
 var Waterline = require('../../');
 
-// helpers
-var isORM = require('../helpers/isORM');
-var isAdapter = require('../helpers/isAdapter');
-var isDatabase = require('../helpers/isDatabase');
-var isModel = require('../helpers/isModel');
+
+// fixtures
+var SimpleORM = require('../fixtures/SimpleORM');
 
 
 describe('integration', function () {
-  describe('DDL (setting up the database)', function () {
+  describe('`Database`: DDL (data definition language- i.e. schema CRUD)', function () {
 
-    it('should not fail w/ normal, declarative usage', function () {
+    var orm, db;
 
-      var orm = Waterline({
-        models: {
-          user: {
-            database: 'default',
-            attributes: {}
-          }
-        },
-        databases: {
-          default: {
-            adapter: 'wl-pretend'
-          }
-        },
-        adapters: {
-          'wl-pretend': {
-            find: function (criteria, cb) { cb('not a real adapter'); }
-          }
-        }
+    before(function () {
+      orm = SimpleORM();
+      db = _(orm.databases).first();
+    });
+
+    describe('db.describe()', function () {
+      it('should exist', function () {
+        assert(typeof db.describe === 'function');
       });
-      assert(isORM(orm));
+      it('should not fail when using callback syntax', function (done) {
+        db.describe(done);
+      });
+      it('should not fail when using .exec() syntax', function (done) {
+        db.describe().exec(done);
+      });
+      it('should not fail when using promise syntax', function (done) {
+        db.describe()
+        .then(function (results) {
+          done();
+        })
+        .error(done);
+      });
+      it('should send back a valid schema description', function (done) {
+        db.describe(function (err, schema) {
+          if (err) return done(err);
+          assert(typeof schema === 'object');
+
+          // TODO: test that schema is in proper format
+
+          return done();
+        });
+      });
     });
 
   });
