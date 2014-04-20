4d716ddca18363de6eecf14b1cfb501e93df9b41
Mike McNeil <mike@balderdash.co>
2014-04-19 22:33:10 -0500

    Made build faster

--- browser/build.sh
+++ browser/build.sh
@@ -1,5 +1,6 @@
 rm browser.min.js.gz
-browserify -e ../index.js -s Waterline -o ./browser.js
+# browserify -e ../index.js -s Waterline -o ./browser.js
+browserify -e ../index.js -o ./browser.js
 du -h browser.js
 uglifyjs browser.js > browser.min.js
 du -h browser.min.js
