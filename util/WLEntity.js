c0ff9de2443a8008a32943cecad22418fa1320b9
Mike McNeil <mike@balderdash.co>
2014-04-19 20:51:59 -0500

    Pulled out private methods from ORM constructor and into two reusable modules- 'constructAll' and 'WLEntity' (a utility class for working with things that have 'identity' properties).

---
+++ util/WLEntity.js
@@ -0,0 +1,150 @@
+/**
+ * Module dependencies
+ */
+
+var _ = require('lodash');
+
+
+
+/**
+ * #WLEntity
+ *
+ * Utility class with static methods for working with most
+ * Waterline classes and instances.  In particular, this
+ * module provides helpful methods for any classes, instances,
+ * or sets of such which key off of an `identity` property.
+ *
+ * (currently most useful for Models, Adapters, and Databases)
+ */
+function WLEntity () {
+
+}
+
+
+/**
+ * @param  {Array} array
+ * @return {Object}
+ * @api private
+ */
+WLEntity.toObject = function (array) {
+  if (_.isPlainObject(array)) return array;
+  if (!array) return {};
+
+  return _.reduce(array, function (memo, item) {
+    // Skip items w/o an identity
+    if (!item.identity) return memo;
+    memo[identity] = item.identity;
+    return memo;
+  }, {});
+};
+
+/**
+ * @param  {Object} obj
+ * @return {Array}
+ * @api private
+ */
+WLEntity.toArray = function (obj) {
+  if (_.isArray(obj)) return obj;
+  if (!obj) return [];
+
+  return _.reduce(obj, function (memo, item, key) {
+    item.identity = item.identity || key;
+    memo.push(item);
+    return memo;
+  }, []);
+};
+
+
+/**
+ * Serialize a single entity `definition` and, optionally,
+ * its `identity` into a standard POJO.
+ *
+ * @param  {[type]} identity   [description]
+ * @param  {[type]} definition [description]
+ * @return {Object}
+ * @api private
+ */
+WLEntity.normalize = function (identity, definition) {
+
+  // `identity` argument is optional
+  if (typeof identity === 'object') {
+    definition = identity;
+    identity = undefined;
+  }
+
+  // `definition` is optional, and should be an object
+  definition = definition || {};
+
+  // Apply `identity` argument to definition, if relevant
+  if (identity) {
+    identity = identity.toLowerCase();
+    definition.identity = identity;
+  }
+
+  return definition;
+};
+
+
+/**
+ * [_buildIdentifier description]
+ * @param  {[type]} things [description]
+ * @param  {[type]} Thing  [description]
+ * @return {[type]}        [description]
+ */
+WLEntity.identifier = function (things, Thing) {
+  /**
+   * [description]
+   * @param  {[type]} identity   [description]
+   * @param  {[type]} definition [description]
+   * @return {[type]}            [description]
+   * @api private
+   */
+  return function (identity, definition) {
+    definition = WLEntity.normalize(identity, definition);
+    definition.orm = this;
+    this[things].push(new Thing(definition));
+    this.refresh();
+    return this;
+  };
+};
+
+/**
+ * [_buildForgetter description]
+ * @param  {[type]} things [description]
+ * @return {[type]}        [description]
+ * @api private
+ */
+WLEntity.forgetter = function (things) {
+
+  /**
+   * [description]
+   * @param  {[type]} identity [description]
+   * @return {[type]}          [description]
+   */
+  return function (identity) {
+    _.remove(this[things], { identity: identity });
+    return this;
+  };
+};
+
+/**
+ * [_buildGetter description]
+ * @param  {[type]} things [description]
+ * @return {[type]}        [description]
+ * @api private
+ */
+WLEntity.getter = function (things) {
+  /**
+   * [description]
+   * @param  {[type]} identity [description]
+   * @return {[type]}          [description]
+   */
+  return function (identity) {
+    return _.find(this[things], { identity: identity });
+  };
+};
+
+
+
+module.exports = WLEntity;
+
---
+++ util/constructAll.js
@@ -0,0 +1,24 @@
+/**
+ * Module dependencies
+ */
+
+var _ = require('lodash');
+
+
+
+/**
+ * "New up" a given array of definitions using the
+ * given Class constructor.
+ *
+ * @param  {Array{Object?}} set
+ * @param {Class.constructor} Class
+ * @return {Array{Class}}
+ * @api private
+ */
+
+module.exports = function _constructAll(set, Class) {
+  return _.map(set, function (item) {
+    if ( item instanceof Class ) return item;
+    else return new Class(item);
+  });
+};
