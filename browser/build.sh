d7b0da97c3c1edfa43dc31ac072018083447b476
Mike McNeil <mike@balderdash.co>
2014-04-19 22:29:49 -0500

    Added stub default adapter, browser compilation stuff

---
+++ browser/build.sh
@@ -0,0 +1,7 @@
+rm browser.min.js.gz
+browserify -e ../index.js -s Waterline -o ./browser.js
+du -h browser.js
+uglifyjs browser.js > browser.min.js
+du -h browser.min.js
+gzip browser.min.js
+du -h browser.min.js.gz
---
+++ wl-memory/index.js
@@ -0,0 +1,29 @@
+/**
+ * Module dependencies
+ */
+
+var WLFilter = require('waterline-criteria');
+
+
+// The `db` variable holds all of the databases
+var db = {};
+// The `currentAutoIncrementValues` variable holds each of their next AI values.
+var currentAutoIncrementValues = {};
+
+// The adapter interface
+module.exports = {
+
+  find: function (criteria, cb) {
+    return cb(null, WLFilter(criteria.from, db, criteria).results);
+  },
+  create: function (values, cb) {
+    cb(new Error('Not implemented yet'));
+  },
+  update: function (criteria, update, cb) {
+    cb(new Error('Not implemented yet'));
+  },
+  destroy: function (criteria, cb) {
+    cb(new Error('Not implemented yet'));
+  }
+
+};
