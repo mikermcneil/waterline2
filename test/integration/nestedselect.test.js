c899986243e2ee5003edc9901939432845d6f24f
Mike McNeil <mike@balderdash.co>
2014-05-12 02:46:22 -0500

    Makes populate..where work.

--- test/integration/nestedselect.test.js
+++ test/integration/nestedselect.test.js
@@ -12,11 +12,12 @@ var PeopleAndTheirCats = require('../fixtures/PeopleAndTheirCats');
 describe('integration', function () {
   describe('nested select query', function () {
 
+
     var orm = PeopleAndTheirCats();
+    var Person = orm.model('person');
 
     it('should return all of the expected parent results', function (done) {
 
-      var Person = orm.model('person');
 
       Person.find({
         where: { id: [1,2] }
@@ -52,8 +53,59 @@ describe('integration', function () {
 
       });
 
-
     });
 
-  });
+
+
+
+    describe('nested select...where', function () {
+
+      var expected;
+
+      before(function (done){
+        Person.find({
+          where: { id: [1,2] }
+        })
+        .exec(function (err, _expected) {
+          if (err) return done(err);
+          expected = _expected;
+          done();
+        });
+      });
+
+      it('should return expected nested results', function (done) {
+        var q =
+        Person.find({
+          where: { id: [1,2] },
+          select: {
+            id: true,
+            name: true,
+            email: true,
+            petCat: {
+              select: {
+                id: true,
+                name: true
+              },
+              where: {
+                numEars: 3
+              }
+            }
+          }
+        })
+        .exec(function (err, persons) {
+          if (err) return done(err);
+
+          // console.log('Vs:', persons);
+          // console.log(q.cache);
+          assert.equal(persons.length, expected.length, require('util').format('Unexpected number of top-level results (expected %d, got %d)', expected.length, persons.length));
+          done();
+        });
+      });
+
+    }); // </describe(nested select...where)>
+
+  });  // </describe(nested select query)>
+
+
+
 });
