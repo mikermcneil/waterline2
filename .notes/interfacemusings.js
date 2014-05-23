d763518c12074e1e933940ec2f190dea67568c19
Mike McNeil <mike@balderdash.co>
2014-05-23 00:25:38 -0500

    Final bit of refactoring

---
+++ .notes/interfacemusings.js
@@ -0,0 +1,31 @@
+/**
+ * @implements {WLEntity}
+ *               • `.refresh()`
+ *               • `.getAdapter()`
+ *               • `.getDatabase()`
+ *               • `.getModel()`
+ *
+ * @implements {Query}
+ *               • `.query()`
+ *               • `.bootstrap()`
+ *               • `.create()`
+ *               • `.createEach()`
+ *               • `.destroy()`
+ *               • `.find()`
+ *               • `.findOne()`
+ *               • `.findOrCreate()`
+ *               • `.populate()`
+ *               • `.update()`
+ *               • `.updateOrCreate()`
+ *
+ * @implements {Transaction}
+ *               • `.transaction()`
+ *
+ * @implements {Physical}
+ *               • `.migrate()`
+ *               • `.describe()`
+ *               • `.define()`
+ *               • `.drop()`
+ *               • `.removeAttr()`
+ *               • `.addAttr()`
+ */
