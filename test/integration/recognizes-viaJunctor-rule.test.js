df382d1f2eecae6f4412012290bdeae8faef9ca0
Mike McNeil <mike@balderdash.co>
2014-06-20 08:08:24 -0500

    viaJunctor 'recognizes' tests

--- test/integration/recognizes-viaJunctor-rule.test.js
+++ test/integration/recognizes-viaJunctor-rule.test.js
@@ -10,30 +10,54 @@ var Waterline = require('root-require')('./');
 
 describe('recognizes-association-rules', function () {
 
-  describe('hasFK', function () {
+  describe('viaJunctor', function () {
     var orm;
     before(function () {
       orm = Waterline().model('Stark', {
         attributes: {
-          petWolf: { model: 'Direwolf' }
+
+          favoriteWolves: { collection: 'direwolf' },
+          raisedWolves: { collection: 'direwolf', via: 'raisedByHumans' }
+
         }
       }).model('Direwolf', {
         attributes: {
-          humanOwner: { model: 'Stark' }
+
+          favoriteHumans: { collection: 'stark' },
+          raisedByHumans: { collection: 'direwolf', via: 'raisedWolves' }
+
         }
       }).refresh();
+
+      //
+      // Note that `favoriteWolves` and `favoriteHumans` are disjoint sets,
+      // whereas `raisedWolves` and `raisedByHumans` are the same set.
+      //
     });
 
-    it('should be built automatically when `model` is present w/ no accompanying collection..via', function () {
+
+    it('should be built automatically when `collection` is present w/o a `via`', function () {
       assert.equal(
-        orm.model('direwolf').getAssociationRule('humanOwner').identity,
-        'hasFK'
+        orm.model('stark').getAssociationRule('favoriteWolves').identity,
+        'viaJunctor'
       );
       assert.equal(
-        orm.model('stark').getAssociationRule('petWolf').identity,
-        'hasFK'
+        orm.model('direwolf').getAssociationRule('favoriteHumans').identity,
+        'viaJunctor'
       );
     });
+
+    it('should be built automatically when `collection`+`via` is present', function () {
+      assert.equal(
+        orm.model('stark').getAssociationRule('raisedWolves').identity,
+        'viaJunctor'
+      );
+      assert.equal(
+        orm.model('direwolf').getAssociationRule('raisedByHumans').identity,
+        'viaJunctor'
+      );
+    });
+
   });
 
 
